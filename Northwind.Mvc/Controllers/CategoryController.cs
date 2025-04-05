747dd9227a24992f0c0cab091c34838a12aecbc5
Ali Nekzad <ali.nekzad.webd24jon@edu.tucsweden.se>
2025-04-05 21:33:56 +0200

    Add project files.

---
+++ Northwind.Mvc/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.EntityModels;
+using Northwind.Mvc.Models;
+
+namespace Northwind.Mvc.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly NorthwindDatabaseContext db;
+
+        public CategoryController(NorthwindDatabaseContext northwindDatabase)
+        {
+            db = northwindDatabase;
+        }
+        public IActionResult Index()
+        {
+            var obj = db.Categories.ToList();
+            return View(obj);
+        }
+        public IActionResult Details(int id)
+        {
+            var obj = db.Categories.FirstOrDefault(x => x.CategoryId == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ Northwind.Mvc/Models/CategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Northwind.Mvc.Models
+{
+    public class CategoryViewModel
+    {
+        public int CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+        public string? Description { get; set; }
+        public byte[]? Picture { get; set; }
+    }
+}
